25b6fee35216d3763ebe0e978a2bf20f5feba9f5
calKU0 <228528@student.uek.krakow.pl>
2023-07-29 18:43:44 +0200

    move functions to database.py file

--- keys.py
+++ keys.py
@@ -1,20 +1,9 @@
 import json
-import oauth2 as oauth
-from datetime import datetime
-
-class authorization:      
-    def consumer():
-        return oauth.Consumer(oauth.secrets("consumer_key"), oauth.secrets("CONSUMER_SECRET"))
     
-    def access_token(user, access_token):
-        pass
-        #operacja na bazie, aby znaleźć access token dla usera
-        #Edit: Prawdopodobnie wywali circular refernce excepion więc ogarnąć inny sposób albo wyjebać tą funkcję
-          
-    def secrets(name):
-        with open ("config.json") as config:
-            secrets = json.load(config)
-            return secrets[name.upper()]
+def secrets(name):
+    with open ("config.json") as config:
+        secrets = json.load(config)
+        return secrets[name.upper()]
         
 
 
